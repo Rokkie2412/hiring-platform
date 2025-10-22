e397ba41bb8cf2a10068c1ac604b0c39403a3151
Ervan Jovian <ervanjovian@gmail.com>
2025-10-22 21:41:52 +0700

    update

--- src/pages/admin/ManageJob.tsx
+++ src/pages/admin/ManageJob.tsx
@@ -269,8 +269,9 @@ const DynamicCandidateTable = (): ReactElement => {
               w-full pl-10 pr-4 py-2
               border border-gray-300
               rounded-lg
-              focus:ring-2 focus:ring-purple-500
+              focus:ring-2 focus:ring-teal-600
               focus:outline-none
+              text-black
             "
           />
         </div>
